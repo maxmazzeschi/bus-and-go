b4aa9bfe28ef744666d7191ca3ceec026eb4beee
maxmazzeschi <max.mazzeschi@gmail.com>
2025-02-24 11:57:49 +0100

    vehicle class added

--- app/app.py
+++ app/app.py
@@ -1,79 +1,33 @@
 from flask import Flask, jsonify, render_template, request
-import time
-import requests
-from google.transit import gtfs_realtime_pb2
-import threading
+from vehicles import Vehicles
 
 app = Flask(__name__)
-
-vehicles = []
-vehicles_lock = threading.Lock()  # For thread safety
+vehicles = Vehicles()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/get_vehicle_positions')
+
+@app.route("/get_vehicle_positions")
 def get_vehicle_positions():
-    update_vehicle_positions()
-    north = float(request.args.get('north', 90))
-    south = float(request.args.get('south', -90))
-    east = float(request.args.get('east', 180))
-    west = float(request.args.get('west', -180))
-    selected_routes = request.args.get('routes')
-    selected_routes = selected_routes.split(',') if selected_routes else []
-    with vehicles_lock:
-        filtered_vehicles = [
-            v for v in vehicles
-            if south <= v['lat'] <= north and west <= v['lon'] <= east and
-            (not selected_routes or v['route_id'] in selected_routes)
-        ]
-    print(f'Filtered vehicles: {len(filtered_vehicles)}')
+    north = float(request.args.get("north", 90))
+    south = float(request.args.get("south", -90))
+    east = float(request.args.get("east", 180))
+    west = float(request.args.get("west", -180))
+    selected_routes = request.args.get("routes")
+    filtered_vehicles = vehicles.get_vehicle_positions(
+        north, south, east, west, selected_routes
+    )
     return jsonify(filtered_vehicles)
 
-@app.route('/get_available_routes')
+
+@app.route("/get_available_routes")
 def get_available_routes():
-    update_vehicle_positions()
-    with vehicles_lock:
-        route_ids = list({v['route_id'] for v in vehicles})
+    route_ids = vehicles.get_available_routes()
     return jsonify(route_ids)
 
 
-def update_vehicle_positions():
-    global vehicles
-    url = 'https://romamobilita.it/sites/default/files/rome_rtgtfs_vehicle_positions_feed.pb'
-    while True:
-        print('Updating vehicle positions...')
-        feed = gtfs_realtime_pb2.FeedMessage()
-        response = requests.get(url)
-        feed.ParseFromString(response.content)
-        new_vehicles = []
-        for entity in feed.entity:
-            if entity.HasField('vehicle'):
-                vehicle_id = entity.vehicle.vehicle.id
-                route_id = entity.vehicle.trip.route_id
-                latitude = entity.vehicle.position.latitude
-                longitude = entity.vehicle.position.longitude
-                new_vehicles.append({
-                    'vehicle_id': vehicle_id,
-                    'route_id': route_id,
-                    'lat': latitude,
-                    'lon': longitude
-                })
-        with vehicles_lock:
-            vehicles = new_vehicles
-        print(f'Updated vehicle positions: {len(vehicles)}')
-        # print(f'Vehicles: {vehicles}')
-        # time.sleep(60)
-
-
-# Start the update in a separate thread
-#thread = threading.Thread(target=update_vehicle_positions)
-#thread.daemon = True
-#thread.start()
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ app/vehicles.py
@@ -0,0 +1,70 @@
+import time
+import requests
+from google.transit import gtfs_realtime_pb2
+import threading
+
+
+class Vehicles:
+
+    def __init__(self):
+        self.vehicles = []
+        self.vehicles_lock = threading.Lock()
+        self.url = "https://romamobilita.it/sites/default/files/rome_rtgtfs_vehicle_positions_feed.pb"
+        self.update_vehicle_positions()
+        self.update_thread = threading.Thread(target=self.update_loop)
+        self.update_thread.daemon = True
+        self.update_thread.start()
+
+    def update_vehicle_positions(self):
+        feed = gtfs_realtime_pb2.FeedMessage()
+        response = requests.get(self.url)
+        feed.ParseFromString(response.content)
+        new_vehicles = []
+        for entity in feed.entity:
+            if entity.HasField("vehicle"):
+                vehicle_id = entity.vehicle.vehicle.id
+                route_id = entity.vehicle.trip.route_id
+                latitude = entity.vehicle.position.latitude
+                longitude = entity.vehicle.position.longitude
+                new_vehicles.append(
+                    {
+                        "vehicle_id": vehicle_id,
+                        "route_id": route_id,
+                        "lat": latitude,
+                        "lon": longitude,
+                    }
+                )
+        with self.vehicles_lock:
+            self.vehicles = new_vehicles
+        print(f"Updated vehicle positions: {len(self.vehicles)}")
+
+    def update_loop(self):
+        while True:
+            self.update_vehicle_positions()
+            time.sleep(60)
+
+    def get_vehicle_positions(self, north, south, east, west, selected_routes):
+        north = float(north)
+        south = float(south)
+        east = float(east)
+        west = float(west)
+        selected_routes = selected_routes.split(",") if selected_routes else []
+        with self.vehicles_lock:
+            filtered_vehicles = [
+                v
+                for v in self.vehicles
+                if south <= v["lat"] <= north
+                and west <= v["lon"] <= east
+                and (not selected_routes or v["route_id"] in selected_routes)
+            ]
+        print(f"Filtered vehicles: {len(filtered_vehicles)}")
+        return filtered_vehicles
+
+    def get_available_routes(self):
+        with self.vehicles_lock:
+            route_ids = list({v["route_id"] for v in self.vehicles})
+        return route_ids
+
+    def get_vehicles(self):
+        with self.vehicles_lock:
+            return self.vehicles
