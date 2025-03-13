b8d19a5eb2ff34c9cc9828c2216121bb0aa631d7
max.mazzeschi <max.mazzeschi@live.com>
2025-03-13 15:52:08 +0100

    Turin added

--- app/GTFSDatasetsProvider/dataset.py
+++ app/GTFSDatasetsProvider/dataset.py
@@ -11,21 +11,24 @@ class Dataset:
         self.src = provider
         self.vehicle_url = self.src["vehicle_positions_url"]
         self.vehicles = Vehicles(self.vehicle_url)
-        response = requests.get(self.src["static_gtfs_url"])
-        temp_filename = tempfile.NamedTemporaryFile(suffix='.zip',
-                                                    delete=False).name
-        with open(temp_filename, 'wb') as file:
-            file.write(response.content)
-        # Extract the ZIP file
-        temp_file_path = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}")
-        with zipfile.ZipFile(temp_filename, 'r') as zip_ref:
-            zip_ref.extractall(temp_file_path)
-        os.remove(temp_filename)
-        #os.removedirs(temp_file_path)
+        static_gtfs_url = self.src["static_gtfs_url"]
+        if static_gtfs_url:
+            response = requests.get(self.src["static_gtfs_url"])
+            temp_filename = tempfile.NamedTemporaryFile(suffix='.zip',
+                                                        delete=False).name
+            with open(temp_filename, 'wb') as file:
+                file.write(response.content)
+            # Extract the ZIP file
+            temp_file_path = os.path.join(tempfile.gettempdir(), f"{uuid.uuid4()}")
+            with zipfile.ZipFile(temp_filename, 'r') as zip_ref:
+                zip_ref.extractall(temp_file_path)
+            os.remove(temp_filename)
+            #os.removedirs(temp_file_path)
 
     def get_available_routes(self):
         return self.vehicles.get_available_routes()
 
     def get_vehicles_positions(self, north, south, east, west, selected_routes):
         return self.vehicles.get_vehicles_positions(north, south,
-                                                   east, west, selected_routes)
+                                                    east, west,
+                                                    selected_routes)
--- app/GTFSDatasetsProvider/datasets_provider.py
+++ app/GTFSDatasetsProvider/datasets_provider.py
@@ -3,7 +3,7 @@ import os
 import threading
 from .dataset import Dataset
 import re
-
+import hashlib
 
 datasets = {}
 datasets_lock = threading.Lock()
@@ -21,6 +21,7 @@ class GTFSDatasetsProvider:
 
     @staticmethod
     def get_dataset(id):
+        print(id)
         with datasets_lock:
             ds = datasets.get(id)
             if ds:
@@ -31,13 +32,14 @@ class GTFSDatasetsProvider:
                     if re.search(r"\.json", os.fsdecode(entry.name)):
                         with open(entry.path) as f:
                             provider = json.load(f)
-                            if provider["id"] == id:
+                            provider_hash = GTFSDatasetsProvider.generate_provider_hash(provider)
+                            if id == provider_hash:
                                 found = True
                                 break
             if found is False:
                 raise NameError
             ds = Dataset(provider)
-            datasets[id] = ds
+            datasets[provider_hash] = ds
             return ds
 
     @staticmethod
@@ -48,7 +50,14 @@ class GTFSDatasetsProvider:
                 if re.search(r"\.json", os.fsdecode(entry.name)):
                     with open(entry.path) as f:
                         provider = json.load(f)
+                        provider_hash = GTFSDatasetsProvider.generate_provider_hash(provider)
                         datasets_list.append(
-                            {"name": provider["name"], "id": provider["id"]}
+                            {"name": provider["name"], "id": provider_hash}
                         )
+        print(datasets_list)
         return datasets_list
+    
+    @staticmethod
+    def generate_provider_hash(provider: dict) -> str:
+        provider_str = json.dumps(provider, sort_keys=True)
+        return hashlib.sha256(provider_str.encode('utf-8')).hexdigest()
