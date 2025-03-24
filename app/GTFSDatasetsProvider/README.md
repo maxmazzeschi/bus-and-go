b6936824d23760abfb2a15f4d7bdab5e72114e4a
max.mazzeschi <max.mazzeschi@live.com>
2025-03-24 16:04:51 +0100

    datasets as separate library

--- app/GTFSDatasetsProvider/README.md
+++
@@ -1,8 +0,0 @@
-#New York
-https://api.mta.info/#/
-##Bus
-https://bt.mta.info/wiki/Developers/Index
-
-
-Standards
-SIRI https://transmodel-cen.eu/index.php/siri/
--- app/GTFSDatasetsProvider/__init__.py
+++
@@ -1,6 +0,0 @@
-# GTFSDatasetProvider/__init__.py
-from .datasets_provider import GTFSDatasetsProvider
-from .vehicles import Vehicles
-from .dataset import Dataset
-
-__all__ = ["GTFSDatasetsProvider", "Vehicles", "Dataset"]
